e9267ba3128230d85dfed4bd49c807170929c35a
zaralX <61970128+zaralX@users.noreply.github.com>
2024-12-28 18:44:53 +0800

    cabinet update

--- layouts/cabinet.vue
+++ layouts/cabinet.vue
@@ -38,8 +38,19 @@ const handleCategory = async (tab: TabsPaneContext, event: Event) => {
     <el-tabs v-model="activeCategory" @tab-click="handleCategory">
       <el-tab-pane v-for="(value, key) in links" :label="value.label" :name="key" :key="key">
         <template #label>
-          <div class="w-48 text-center">
-            {{value.label}}
+          <div class="w-56 text-center flex justify-center items-center">
+            <el-dropdown trigger="click" class="w-full">
+              <div class="select-none w-full flex justify-center items-center">
+                <p>{{value.label}}</p>
+                <i class="pi pi-chevron-down absolute right-0 text-xs pr-2"></i>
+              </div>
+              <template #dropdown>
+<!--                <el-dropdown-menu>-->
+<!--                  <el-dropdown-item>Главная</el-dropdown-item>-->
+<!--                  <el-dropdown-item>Создать товар</el-dropdown-item>-->
+<!--                </el-dropdown-menu>-->
+              </template>
+            </el-dropdown>
           </div>
         </template>
       </el-tab-pane>
@@ -50,6 +61,8 @@ const handleCategory = async (tab: TabsPaneContext, event: Event) => {
 </div>
 </template>
 
-<style>
-
+<style scoped>
+.el-dropdown {
+  color: inherit;
+}
 </style>
