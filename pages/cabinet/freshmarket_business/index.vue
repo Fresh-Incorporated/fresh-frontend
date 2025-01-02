5ef082f3ebc9f96255fc6d33a8fa359307258956
zaralX <61970128+zaralX@users.noreply.github.com>
2025-01-03 05:58:29 +0800

    create shop

--- pages/cabinet/freshmarket_business/index.vue
+++ pages/cabinet/freshmarket_business/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import FMCabinetFragmentSells from "~/components/cabinet/freshmarket/FMCabinetFragmentSells.vue";
+import CreateShopMenu from "~/components/cabinet/freshmarket/CreateShopMenu.vue";
 
 definePageMeta({
   layout: 'cabinet'
@@ -16,40 +17,45 @@ const shops = [{
   label: "l2",
   value: "v2"
 }]
+
+const openedCreateMenu = ref(false);
 </script>
 
 <template>
-  <div class="hidden">
-    <div class="flex items-center gap-2 mb-4">
-      <p>Выбран магазин:</p>
-      <el-select
-          v-model="selectedShop"
-          placeholder="Выберите магазин"
-          style="width: 200px"
-      >
-        <el-option
-            v-for="item in shops"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value"
-        />
-      </el-select>
-    </div>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-8">
-      <FMCabinetFragmentSells/>
-      <div class="bg-neutral-900 rounded-xl shadow-lg border border-neutral-800 h-64">
-
+  <div>
+    <CreateShopMenu v-model="openedCreateMenu" />
+    <div class="hidden">
+      <div class="flex items-center gap-2 mb-4">
+        <p>Выбран магазин:</p>
+        <el-select
+            v-model="selectedShop"
+            placeholder="Выберите магазин"
+            style="width: 200px"
+        >
+          <el-option
+              v-for="item in shops"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+          />
+        </el-select>
       </div>
-      <div class="bg-neutral-900 rounded-xl shadow-lg border border-neutral-800 h-64">
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-8">
+        <FMCabinetFragmentSells/>
+        <div class="bg-neutral-900 rounded-xl shadow-lg border border-neutral-800 h-64">
 
+        </div>
+        <div class="bg-neutral-900 rounded-xl shadow-lg border border-neutral-800 h-64">
+
+        </div>
       </div>
     </div>
-  </div>
-  <div class="flex flex-col items-center justify-center">
-    <p class="text-xl">У вас нет ни единого магазина</p>
-    <el-button type="success" plain>
-      <p><i class="pi pi-plus text-xs"></i>  Создать</p>
-    </el-button>
+    <div class="flex flex-col items-center justify-center">
+      <p class="text-xl">У вас нет ни единого магазина</p>
+      <el-button @click="openedCreateMenu = true" type="success" plain>
+        <p><i class="pi pi-plus text-xs"></i>  Создать</p>
+      </el-button>
+    </div>
   </div>
 </template>
 
