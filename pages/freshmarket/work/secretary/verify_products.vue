6d629342b7ad7e1aeb1ac4aa3c5055b4f5125ad9
zaralX <61970128+zaralX@users.noreply.github.com>
2025-01-05 07:03:23 +0800

    product verification

--- pages/freshmarket/work/secretary/verify_products.vue
+++ pages/freshmarket/work/secretary/verify_products.vue
@@ -1,13 +1,35 @@
 <script setup lang="ts">
+import {http} from "~/composables/useHttp"
+
 definePageMeta({
   layout: 'freshmarketwork'
 })
+
+const products = ref([])
+
+onMounted(async () => {
+  products.value = (await http.get(`freshmarket/work/secretary/products`))?.data;
+})
+
+const accept = async (id: number) => {
+  await http.post(`freshmarket/work/secretary/product/${id}/accept`)
+}
+
+const decline = async (id: number) => {
+  await http.post(`freshmarket/work/secretary/product/${id}/decline`)
+}
 </script>
 
 <template>
-<div>
-  123
-</div>
+  <div>
+    <div v-for="product in products" class="flex flex-col">
+      <p>{{product.name}}</p>
+      <p>{{product.description}}</p>
+      <p>{{product}}</p>
+      <el-button @click="accept(product.id)">Подтвердить</el-button>
+      <el-button @click="decline(product.id)">Отклонить</el-button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
