e94e811c0b80813a31110004fb6217a45bf7327f
zaralX <61970128+zaralX@users.noreply.github.com>
2025-01-05 06:53:54 +0800

    shop verification

--- pages/freshmarket/work/secretary/verify_shops.vue
+++ pages/freshmarket/work/secretary/verify_shops.vue
@@ -1,12 +1,33 @@
 <script setup lang="ts">
+import {http} from "~/composables/useHttp"
+
 definePageMeta({
   layout: 'freshmarketwork'
 })
+
+const shops = ref([])
+
+onMounted(async () => {
+  shops.value = (await http.get(`freshmarket/work/secretary/shops`))?.data;
+})
+
+const accept = async (id: number) => {
+  await http.post(`freshmarket/work/secretary/shop/${id}/accept`)
+}
+
+const decline = async (id: number) => {
+  await http.post(`freshmarket/work/secretary/shop/${id}/decline`)
+}
 </script>
 
 <template>
 <div>
-  123
+  <div v-for="shop in shops" class="flex flex-col">
+    <p>{{shop.name}}</p>
+    <p>{{shop.description}}</p>
+    <el-button @click="accept(shop.id)">Подтвердить</el-button>
+    <el-button @click="decline(shop.id)">Отклонить</el-button>
+  </div>
 </div>
 </template>
 
