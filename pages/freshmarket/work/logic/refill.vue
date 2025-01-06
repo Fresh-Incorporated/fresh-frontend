d75561ff9699322e1c9ead58edf65e72005120b3
zaralX <61970128+zaralX@users.noreply.github.com>
2025-01-07 03:25:59 +0800

    logic refill

--- pages/freshmarket/work/logic/refill.vue
+++ pages/freshmarket/work/logic/refill.vue
@@ -1,13 +1,85 @@
 <script setup lang="ts">
+import {http} from "~/composables/useHttp"
+import FMProductHistory from "~/components/freshmarket/work/FMProductHistory.vue";
+
 definePageMeta({
   layout: 'freshmarketwork'
 })
+
+const {user} = useUser()
+const refillCount = ref(0)
+const refillMessage = ref("")
+const refillDialog = ref(false)
+
+const selectedId = ref(0)
+
+const products = ref([])
+
+onMounted(async () => {
+  products.value = (await http.get(`freshmarket/work/logic/list/refill`))?.data;
+  for (const product of products.value) {
+    product?.history?.reverse()
+  }
+})
+
+const accept = async (id: number) => {
+  await http.post(`freshmarket/work/logic/product/${id}/refill`)
+}
+
+const confirm = async () => {
+  await http.post(`freshmarket/work/logic/product/${selectedId.value}/refill/end`, {
+    add: refillCount.value,
+    message: refillMessage.value.length === 0 ? null : refillMessage.value,
+  })
+}
 </script>
 
 <template>
-<div>
-  123
-</div>
+  <div class="w-full">
+    <div class="grid grid-cols-5 gap-4">
+      <el-dialog
+          v-model="refillDialog"
+          title="Пополнение товара"
+          width="800"
+      >
+        <div>
+          <el-input-number
+              v-model="refillCount"
+              placeholder="Кол-во"
+              :min="0"
+              :max="27 * 64 * 10"
+          />
+          <el-input
+              v-model="refillMessage"
+              type="textarea"
+              placeholder="Напишите сообщение (при надобности).."
+              :maxlength="120"
+              show-word-limit
+              :formatter="(value) => `${value}`.replace(/[\r\n]+/g, '')"
+              :parser="(value) => value.replace(/[\r\n]+/g, '')"
+          />
+          <el-button @click="confirm">
+            Завершить
+          </el-button>
+        </div>
+      </el-dialog>
+      <div v-for="product in products" class="flex flex-col">
+        <img :src="product.icon" class="w-full aspect-square" alt="">
+        <p class="font-semibold">{{ product.name }}</p>
+        <p>{{ product.description }}</p>
+        <p>Кол-во в стаке: {{ product.stack_count }}</p>
+        <p>Кол-во слотов: {{ product.slots_count }}</p>
+        <p>Ячейка: {{ product.cell?.letter }}-{{ product.cell?.number }}</p>
+        <p>Склад: {{ product.cell?.location?.name }}</p>
+        <el-button v-if="product?.currentRefiller?.id == user?.id"
+                   @click="selectedId = product.id; refillDialog = true">Открыть меню
+        </el-button>
+        <el-button @click="accept(product.id)"
+                   :disabled="product.refill_status != 2 || product?.currentRefiller != null">Принять
+        </el-button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
