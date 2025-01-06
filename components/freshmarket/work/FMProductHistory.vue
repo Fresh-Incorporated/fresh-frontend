11ddc96e143d7d9932e83de08a265f580c0bb421
zaralX <61970128+zaralX@users.noreply.github.com>
2025-01-06 09:39:04 +0800

    product history

---
+++ components/freshmarket/work/FMProductHistory.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import {http} from "~/composables/useHttp"
+
+const model = defineModel()
+const props = defineProps({
+  history: Array,
+})
+</script>
+
+<template>
+  <div>
+    <el-drawer
+        v-model="model"
+        title="История товара"
+        direction="rtl"
+    >
+      <el-timeline style="max-width: 600px">
+        <el-timeline-item
+            v-for="(activity, index) in history"
+            :key="index"
+            :timestamp="activity.createdAt"
+            :type="activity.action_type === 'created' ? 'success' :
+                   activity.action_type === 'accepted' ? 'success' :
+                   activity.action_type === 'declined' ? 'danger' : ''"
+        >
+          <div class="transform -translate-y-1">
+            <div v-if="activity.action_type === 'created'">
+              <p class="text-lg font-medium text-green-500">Товар создан</p>
+              <div class="flex gap-2">
+                <p>Пользователь:</p>
+                <img :src="useXIS().getFullFace(activity.user.uuid)" class="w-4 h-4" alt="">
+                <p>{{activity.user.nickname}}</p>
+              </div>
+              <p>{{activity.data}}</p>
+            </div>
+            <div v-else-if="activity.action_type === 'declined'">
+              <p class="text-lg font-medium text-red-500">Товар отклонён</p>
+              <div class="flex gap-2">
+                <p>Пользователь:</p>
+                <img :src="useXIS().getFullFace(activity.user.uuid)" class="w-4 h-4" alt="">
+                <p>{{activity.user.nickname}}</p>
+              </div>
+              <p>{{activity.message}}</p>
+            </div>
+            <div v-if="activity.action_type === 'accepted'">
+              <p class="text-lg font-medium text-green-500">Товар проверен</p>
+              <div class="flex gap-2">
+                <p>Пользователь:</p>
+                <img :src="useXIS().getFullFace(activity.user.uuid)" class="w-4 h-4" alt="">
+                <p>{{activity.user.nickname}}</p>
+              </div>
+            </div>
+          </div>
+        </el-timeline-item>
+      </el-timeline>
+    </el-drawer>
+  </div>
+</template>
--- pages/freshmarket/work/secretary/verify_products.vue
+++ pages/freshmarket/work/secretary/verify_products.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import {http} from "~/composables/useHttp"
+import FMProductHistory from "~/components/freshmarket/work/FMProductHistory.vue";
 
 definePageMeta({
   layout: 'freshmarketwork'
@@ -8,12 +9,17 @@ definePageMeta({
 const declineMessage = ref("")
 const declineDialog = ref(false)
 
+const showProductHistory = ref(false)
+
 const selectedId = ref(0)
 
 const products = ref([])
 
 onMounted(async () => {
   products.value = (await http.get(`freshmarket/work/secretary/products`))?.data;
+  for (const product of products.value) {
+    product?.history?.reverse()
+  }
 })
 
 const accept = async (id: number) => {
@@ -28,37 +34,41 @@ const decline = async () => {
 </script>
 
 <template>
-  <div class="grid grid-cols-5 gap-4">
-    <el-dialog
-        v-model="declineDialog"
-        title="Отклонение товара"
-        width="800"
-    >
-      <div>
-        <el-input
-            v-model="declineMessage"
-            type="textarea"
-            placeholder="Напишите причину отклонения.."
-            :maxlength="120"
-            show-word-limit
-            :formatter="(value) => `${value}`.replace(/[\r\n]+/g, '')"
-            :parser="(value) => value.replace(/[\r\n]+/g, '')"
-        />
-        <el-button @click="decline">
-          Отклонить
-        </el-button>
+  <div class="w-full">
+    <FMProductHistory v-model="showProductHistory" :history="products?.find(product => product.id === selectedId)?.history" />
+    <div class="grid grid-cols-5 gap-4">
+      <el-dialog
+          v-model="declineDialog"
+          title="Отклонение товара"
+          width="800"
+      >
+        <div>
+          <el-input
+              v-model="declineMessage"
+              type="textarea"
+              placeholder="Напишите причину отклонения.."
+              :maxlength="120"
+              show-word-limit
+              :formatter="(value) => `${value}`.replace(/[\r\n]+/g, '')"
+              :parser="(value) => value.replace(/[\r\n]+/g, '')"
+          />
+          <el-button @click="decline">
+            Отклонить
+          </el-button>
+        </div>
+      </el-dialog>
+      <div v-for="product in products" class="flex flex-col">
+        <img :src="product.icon" class="w-full aspect-square" alt="">
+        <p class="font-semibold">{{product.name}}</p>
+        <p>{{product.description}}</p>
+        <p>Кол-во в стаке: {{product.stack_count}}</p>
+        <p>Кол-во слотов: {{product.slots_count}}</p>
+        <p>Ячейка: {{product.cell?.letter}}-{{product.cell?.number}}</p>
+        <p>Склад: {{product.cell?.location?.name}}</p>
+        <el-button @click="selectedId = product.id; showProductHistory = true">Показать историю товара [{{product.history.length}}шт]</el-button>
+        <el-button @click="accept(product.id)">Подтвердить</el-button>
+        <el-button @click="selectedId = product.id; declineDialog = true">Отклонить</el-button>
       </div>
-    </el-dialog>
-    <div v-for="product in products" class="flex flex-col">
-      <img :src="product.icon" class="w-full aspect-square" alt="">
-      <p class="font-semibold">{{product.name}}</p>
-      <p>{{product.description}}</p>
-      <p>Кол-во в стаке: {{product.stack_count}}</p>
-      <p>Кол-во слотов: {{product.slots_count}}</p>
-      <p>Ячейка: {{product.cell?.letter}}-{{product.cell?.number}}</p>
-      <p>Склад: {{product.cell?.location?.name}}</p>
-      <el-button @click="accept(product.id)">Подтвердить</el-button>
-      <el-button @click="selectedId = product.id; declineDialog = true">Отклонить</el-button>
     </div>
   </div>
 </template>
