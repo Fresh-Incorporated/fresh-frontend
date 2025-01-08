50dde2c148cd3e7405ab6802c3baaee3ff0e396d
zaralX <61970128+zaralX@users.noreply.github.com>
2025-01-08 13:30:13 +0800

    order confirm

--- pages/freshmarket/orders.vue
+++ pages/freshmarket/orders.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
+import {http} from "~/composables/useHttp"
+
 const {orders} = useUser();
 
+const confirm = async (id: number) => {
+  await http.post(`/freshmarket/order/${id}/confirm`)
+}
 </script>
 
 <template>
 <div>
   <div v-for="order in orders?.orders">
     {{order}}
+    <el-button v-if="order.status === 4" @click="confirm(order.id)">Подтвердить получение</el-button>
   </div>
 </div>
 </template>
