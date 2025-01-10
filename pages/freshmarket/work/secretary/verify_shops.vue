49064377930eb4d32a8e65ff3e08e92087555c8f
zaralX <61970128+zaralX@users.noreply.github.com>
2025-01-10 12:47:40 +0800

    updated shop verification page

--- pages/freshmarket/work/secretary/verify_shops.vue
+++ pages/freshmarket/work/secretary/verify_shops.vue
@@ -21,13 +21,57 @@ const decline = async (id: number) => {
 </script>
 
 <template>
-<div class="grid grid-cols-5 gap-4">
+<div class="grid grid-cols-5 gap-4 p-4">
   <div v-for="shop in shops" class="flex flex-col">
     <img :src="shop.icon" class="w-full aspect-square" alt="">
-    <p class="font-semibold">{{shop.name}}</p>
-    <p>{{shop.description}}</p>
-    <el-button @click="accept(shop.id)">Подтвердить</el-button>
-    <el-button @click="decline(shop.id)">Отклонить</el-button>
+    <p>Название: <strong>{{shop.name}}</strong></p>
+    <p>Описание: {{shop.description?.length > 0 ? '' : 'Отсутствует'}}</p>
+    <div class="w-full flex">
+      <el-popconfirm
+          confirm-button-text="Подтвердить"
+          cancel-button-text="Отмена"
+          hide-icon
+          title="Вы уверены что хотите подтвердить этот магазин?"
+          @confirm="accept(shop.id)"
+          :width="300"
+      >
+        <template #reference>
+          <el-button class="w-full" type="success" plain>Подтвердить</el-button>
+        </template>
+        <template #actions="{ confirm, cancel }">
+          <el-button size="small" @click="cancel">Отмена</el-button>
+          <el-button
+              type="danger"
+              size="small"
+              @click="confirm"
+          >
+            Подтвердить
+          </el-button>
+        </template>
+      </el-popconfirm>
+      <el-popconfirm
+          confirm-button-text="Отклонить"
+          cancel-button-text="Отмена"
+          hide-icon
+          title="Вы уверены что хотите отклонить этот магазин?"
+          @confirm="decline(shop.id)"
+          :width="300"
+      >
+        <template #reference>
+          <el-button class="w-full" type="danger" plain>Отклонить</el-button>
+        </template>
+        <template #actions="{ confirm, cancel }">
+          <el-button size="small" @click="cancel">Отмена</el-button>
+          <el-button
+              type="danger"
+              size="small"
+              @click="confirm"
+          >
+            Отклонить
+          </el-button>
+        </template>
+      </el-popconfirm>
+    </div>
   </div>
 </div>
 </template>
