da0a6b787e7f86b5243bb6d225c9d37598b43d35
zaralX <61970128+zaralX@users.noreply.github.com>
2025-01-14 19:08:39 +0800

    old orders

--- components/freshmarket/FMOrderHistory.vue
+++ components/freshmarket/FMOrderHistory.vue
@@ -21,7 +21,7 @@ const props = defineProps({
             :timestamp="activity.createdAt"
             :type="activity.action_type === 'created' ? 'success' :
                    activity.action_type === 'paid' ? 'success' :
-                   activity.action_type === 'declined' ? 'danger' : ''"
+                   activity.action_type === 'confirmed' ? 'success' : 'primary'"
         >
           <div class="transform -translate-y-1">
             <div v-if="activity.action_type === 'created'">
@@ -35,6 +35,51 @@ const props = defineProps({
             </div>
             <div v-else-if="activity.action_type === 'paid'">
               <p class="text-lg font-medium text-green-500">Заказ оплачен</p>
+              <div class="flex gap-2">
+                <p>Пользователь:</p>
+                <img :src="useXIS().getFullFace(activity.user.uuid)" class="w-4 h-4" alt="">
+                <p>{{activity.user.nickname}}</p>
+              </div>
+              <p>{{activity.data}}</p>
+            </div>
+            <div v-else-if="activity.action_type === 'collect_picked'">
+              <p class="text-lg font-medium text-blue-400">Сбор заказа</p>
+              <div class="flex gap-2">
+                <p>Логист:</p>
+                <img :src="useXIS().getFullFace(activity.user.uuid)" class="w-4 h-4" alt="">
+                <p>{{activity.user.nickname}}</p>
+              </div>
+              <p>{{activity.data}}</p>
+            </div>
+            <div v-else-if="activity.action_type === 'collect_finished'">
+              <p class="text-lg font-medium text-blue-400">Заказ собран</p>
+              <div class="flex gap-2">
+                <p>Логист:</p>
+                <img :src="useXIS().getFullFace(activity.user.uuid)" class="w-4 h-4" alt="">
+                <p>{{activity.user.nickname}}</p>
+              </div>
+              <p>{{activity.data}}</p>
+            </div>
+            <div v-else-if="activity.action_type === 'deliver_started'">
+              <p class="text-lg font-medium text-blue-400">Принят курьером</p>
+              <div class="flex gap-2">
+                <p>Курьер:</p>
+                <img :src="useXIS().getFullFace(activity.user.uuid)" class="w-4 h-4" alt="">
+                <p>{{activity.user.nickname}}</p>
+              </div>
+              <p>{{activity.data}}</p>
+            </div>
+            <div v-else-if="activity.action_type === 'deliver_finished'">
+              <p class="text-lg font-medium text-blue-400">Доставлен</p>
+              <div class="flex gap-2">
+                <p>Курьер:</p>
+                <img :src="useXIS().getFullFace(activity.user.uuid)" class="w-4 h-4" alt="">
+                <p>{{activity.user.nickname}}</p>
+              </div>
+              <p>{{activity.data}}</p>
+            </div>
+            <div v-else-if="activity.action_type === 'confirmed'">
+              <p class="text-lg font-medium text-green-500">Подтверждено получение</p>
               <div class="flex gap-2">
                 <p>Пользователь:</p>
                 <img :src="useXIS().getFullFace(activity.user.uuid)" class="w-4 h-4" alt="">
--- pages/freshmarket/orders.vue
+++ pages/freshmarket/orders.vue
@@ -54,6 +54,44 @@ const history = ref([])
         </div>
       </div>
     </div>
+    <h2 class="text-xl font-onest mt-8">Вы уже заказывали</h2>
+    <div class="grid grid-cols-1 xl:grid-cols-2 gap-4 mt-1">
+      <div class="bg-neutral-900 rounded-lg shadow-lg border border-neutral-800" v-for="order in orders?.orders.filter(o => o.status == 5).sort((a, b) => a.status < b.status ? -1 : 1)">
+        <div class="p-2">
+          <div class="flex">
+            <p class="font-onest text-neutral-500 font-medium"><i class="pi pi-warehouse text-sm"></i>  ПУНКТ ВЫДАЧИ ЗАКАЗОВ</p>
+            <div class="flex-1"></div>
+            <div>
+              <div v-if="order?.status == 0" class="bg-neutral-800/[0.25] border border-neutral-800 rounded-lg px-2 py-0.5 text-sm text-neutral-300">Ожидает принятия</div>
+              <div v-else-if="order?.status == 1" class="bg-blue-500/[0.25] border border-blue-500/[0.75] rounded-lg px-2 py-0.5 text-sm text-blue-300">Собираем</div>
+              <div v-else-if="order?.status == 2" class="bg-yellow-800/[0.25] border border-yellow-800 rounded-lg px-2 py-0.5 text-sm text-orange-400">Ищем курьера</div>
+              <div v-else-if="order?.status == 3" class="bg-amber-800/[0.25] border border-amber-800 rounded-lg px-2 py-0.5 text-sm text-orange-300">Доставляем</div>
+              <div v-else-if="order?.status == 4" class="bg-green-500/[0.25] border border-green-500 rounded-lg px-2 py-0.5 text-sm text-green-300">Доставлен</div>
+            </div>
+          </div>
+          <p class="text-neutral-200">Город: {{order?.branch?.city}}</p>
+          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 my-2">
+            <div v-for="product in order?.products" class="border border-neutral-800 rounded-lg flex gap-2">
+              <div class="w-12 h-12">
+                <img :src="orders?.products?.find(p => p.id == product.id)?.icon" class="w-12 h-12" alt="">
+              </div>
+              <div class="w-full truncate relative">
+                <p class="truncate">{{orders?.products?.find(p => p.id == product.id)?.name}}</p>
+                <p class="text-sm truncate absolute right-2 bottom-1 text-neutral-400">{{product?.count}} шт.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="border-t border-neutral-800 p-2 flex gap-4">
+          <p>Цена: <strong class="text-blue-500">{{order?.price}} АР</strong></p>
+          <div class="flex-1"></div>
+          <p class="text-neutral-300">{{order?.data?.products?.reduce((sum, product) => sum + product.count, 0)}} товар</p>
+          <el-button v-if="order.status === 4" @click="confirm(order.id)" size="small" type="success" plain>Подтвердить получение</el-button>
+          <el-button @click="history = order?.history?.reverse(); openedHistory = true" size="small" type="info" text>История</el-button>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 </template>
