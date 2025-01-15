1fdcfed90f027a6639e184cc71f6e651e620a397
zaralX <61970128+zaralX@users.noreply.github.com>
2025-01-15 18:42:46 +0800

    index update

--- pages/index.vue
+++ pages/index.vue
@@ -8,19 +8,23 @@ import IndexHead from "~/components/index/IndexHead.vue";
   <IndexHead />
   <div class="my-16 flex flex-col justify-center items-center gap-16">
     <div class="w-full grid grid-cols-2 gap-4 lg:px-64 h-96">
-      <div class="w-full">
+      <div class="w-full flex flex-col justify-center">
         <h2 class="text-3xl font-rubik font-semibold">О нас</h2>
         <p class="font-light text-base">Fresh Inc. — это инновационный IT-проект, созданный для воплощения самых амбициозных идей и предоставления уникальных решений для пользователей сервера СПм.
           <br> Наша миссия — объединить технологии, креативность и практичность, чтобы создавать удобные, современные и полезные инструменты для всех участников экосистемы.</p>
       </div>
-      <div class="w-full flex justify-center items-center">Изображения нет...</div>
+      <div class="w-full flex justify-center items-center">
+        <img class="w-96" src="/index1.png" alt="">
+      </div>
     </div>
     <div class="w-full grid grid-cols-2 gap-4 lg:px-64 h-96">
-      <div class="w-full flex justify-center items-center">Изображения нет...</div>
+      <div class="w-full flex justify-center items-center">
+        <img class="w-96" src="/index2.png" alt="">
+      </div>
       <div class="w-full">
         <h2 class="text-3xl font-rubik font-semibold text-right">FreshMarket</h2>
-        <p class="font-light text-base">FreshMarket — Место, где пользователи могут покупать, продавать товары, создавая активную и динамичную экономику.</p>
-        <p class="font-light text-base">Начните продавать СЕЙЧАС или станьте новым продавцом!</p>
+        <p class="font-light text-base text-right">FreshMarket — Место, где пользователи могут покупать, продавать товары, создавая активную и динамичную экономику.</p>
+        <p class="font-light text-base text-right">Начните продавать СЕЙЧАС или станьте новым продавцом!</p>
       </div>
     </div>
 <!--    <NuxtLink to="/freshmarket">-->
