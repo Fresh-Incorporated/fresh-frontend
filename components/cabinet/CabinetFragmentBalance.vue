fccb906306b74b07c0dc1ee58ca57565c3df59f9
zaralX <61970128+zaralX@users.noreply.github.com>
2025-04-12 21:22:16 +0800

    spw withdraw

--- components/cabinet/CabinetFragmentBalance.vue
+++ components/cabinet/CabinetFragmentBalance.vue
@@ -6,6 +6,10 @@ const {user, updateUser} = useUser()
 const openedDepositDialog = ref(false)
 const depositValue = ref(1)
 
+const openedWithdrawDialog = ref(false)
+const withdrawCard = ref("")
+const withdrawValue = ref(1)
+
 const deposit = async () => {
   const response = await http.get("users/@me/deposit", {
     params: {
@@ -22,6 +26,14 @@ const deposit = async () => {
     }
   })
   openedDepositDialog.value = false;
+}
+
+const withdraw = async () => {
+  const response = await http.post("users/@me/withdraw", {
+    receiver: withdrawCard.value,
+    amount: withdrawValue.value,
+  })
+  openedWithdrawDialog.value = false;
 }
 
 async function handleMessage(event: MessageEvent) {
@@ -55,6 +67,21 @@ onBeforeUnmount(() => {
         </div>
       </template>
     </el-dialog>
+    <el-dialog
+        v-model="openedWithdrawDialog"
+        title="Пополнение баланса"
+        width="300"
+    >
+      <el-input-number v-model="withdrawValue" placeholder="Value" :min="1" :max="1728" />
+      <el-input v-model="withdrawCard" placeholder="Value" :minlength="5" :maxlength="5" />
+      <template #footer>
+        <div class="flex justify-end">
+          <el-button type="primary" @click="withdraw">
+            Вывести
+          </el-button>
+        </div>
+      </template>
+    </el-dialog>
     <div class="absolute w-12 h-12 bg-sky-900 blur-2xl"></div>
     <div class="absolute w-40 h-40 bottom-0 right-0 translate-x-10 translate-y-10 rotate-8">
       <img class="w-full h-full opacity-50" src="https://img.zaralx.ru/v1/minecraft/deepslate_diamond_ore" alt="">
@@ -71,7 +98,7 @@ onBeforeUnmount(() => {
       <div class="relative w-full mt-2">
         <div class="absolute w-full flex justify-center gap-2">
           <button @click="openedDepositDialog = true" class="border w-6 h-6 flex justify-center items-center text-xs rounded-md shadow-lg border-green-600 text-green-600 hover:text-white hover:bg-green-600 duration-200"><i class="pi pi-arrow-up"></i></button>
-          <button class="border w-6 h-6 flex justify-center items-center text-xs rounded-md shadow-lg border-red-600 text-red-600 hover:text-white hover:bg-red-600 duration-200"><i class="pi pi-arrow-down"></i></button>
+          <button @click="openedWithdrawDialog = true" class="border w-6 h-6 flex justify-center items-center text-xs rounded-md shadow-lg border-red-600 text-red-600 hover:text-white hover:bg-red-600 duration-200"><i class="pi pi-arrow-down"></i></button>
         </div>
       </div>
     </div>
