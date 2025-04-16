60c3bba7b872cd2540d53bc9ae5a9850ac94826a
zaralX <61970128+zaralX@users.noreply.github.com>
2025-04-16 22:55:50 +0800

    16 | Search

--- pages/freshmarket/index.vue
+++ pages/freshmarket/index.vue
@@ -9,7 +9,10 @@ const productsModel = ref({
   loading: false
 });
 
+const searchSeed = Date.now()
 const isAllLoaded = ref(false)
+const search = ref("")
+const sort = ref("relevance")
 
 onMounted(async () => await load())
 
@@ -21,7 +24,14 @@ const load = async () => {
 
   try {
     const offset = productsModel.value.products.length;
-    const newProducts = await http.get(`freshmarket/products?offset=${offset}`);
+    const newProducts = await http.get(`freshmarket/products`, {
+      params: {
+        offset: offset,
+        seed: searchSeed,
+        search: search.value,
+        sort: sort.value,
+      }
+    });
 
     if (newProducts.data.length == 0) {
       isAllLoaded.value = true;
@@ -38,13 +48,55 @@ const load = async () => {
     productsModel.value = {...productsModel.value, loading: false};
   }
 };
+
+const changedFilters = async () => {
+  productsModel.value.products = [];
+  isAllLoaded.value = false;
+  await load();
+}
+
+const searchInput = async () => {
+  const searchOld = search.value;
+  setTimeout(async () => {
+    if (searchOld == search.value) {
+      await changedFilters();
+    }
+  }, 1000)
+}
 </script>
 
 <template>
   <div v-infinite-scroll="load" infinite-scroll-immediate="false">
     <ProjectHead></ProjectHead>
     <FMCartButton/>
-    <FMProductsList v-model="productsModel"/>
+    <div class="md:w-11/12 mx-auto mt-3">
+      <el-affix :offset="55">
+        <div class="bg-neutral-900/[.9] backdrop-blur rounded-b-lg shadow-lg p-2 flex gap-2 border border-neutral-800">
+          <el-input placeholder="Поиск по названию" v-model="search" @input="searchInput" @change="changedFilters" />
+          <el-select @change="changedFilters"
+              v-model="sort"
+              placeholder="Сортировка по цене"
+              style="width: 240px"
+          >
+            <el-option
+                label="Сортировка по цене"
+                value="relevance"
+            />
+            <el-option
+                label="Сначала дорогие"
+                value="expensive"
+            />
+            <el-option
+                label="Сначала дешевые"
+                value="cheap"
+            />
+          </el-select>
+        </div>
+      </el-affix>
+    </div>
+    <transition name="hide">
+      <FMProductsList v-if="productsModel.products.length > 0" v-model="productsModel"/>
+    </transition>
     <transition>
       <div v-if="isAllLoaded && productsModel.products.length > 0"
            class="transform duration-500 flex justify-center my-6 text-xl items-center gap-2">
@@ -69,5 +121,17 @@ const load = async () => {
 .v-leave-to {
   opacity: 0;
   @apply scale-0;
+}
+
+.hide-enter-active,
+.hide-leave-active {
+  transition: all 0.5s ease;
+  @apply scale-y-100;
+}
+
+.hide-enter-from,
+.hide-leave-to {
+  opacity: 0;
+  @apply scale-y-0;
 }
 </style>
