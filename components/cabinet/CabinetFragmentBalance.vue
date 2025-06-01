90367c6c9169db1cad9a763e59254e88b1720e6a
zaralX <61970128+zaralX@users.noreply.github.com>
2025-06-01 21:47:53 +0800

    49 | img.zaralx.ru links -> assets.zaralx.ru

--- components/cabinet/CabinetFragmentBalance.vue
+++ components/cabinet/CabinetFragmentBalance.vue
@@ -98,7 +98,7 @@ onBeforeUnmount(() => {
     </el-dialog>
     <div class="absolute w-12 h-12 bg-sky-500 dark:bg-sky-900 blur-2xl"></div>
     <div class="absolute w-40 h-40 bottom-0 right-0 translate-x-10 translate-y-10 rotate-8">
-      <img class="w-full h-full opacity-50" src="https://img.zaralx.ru/v1/minecraft/deepslate_diamond_ore" alt="">
+      <img class="w-full h-full opacity-50" src="https://assets.zaralx.ru/api/v1/minecraft/vanilla/item/deepslate_diamond_ore/icon" alt="">
     </div>
     <div class="absolute w-12 h-12 bg-sky-500 dark:bg-sky-900 blur-2xl bottom-0 right-0"></div>
     <div class="flex flex-col justify-center items-center w-full h-full font-medium absolute z-10">
