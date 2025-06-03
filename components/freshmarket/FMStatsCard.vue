5d1c5d2067984a28f71bc8f715166e4bed4dcb87
zaralX <61970128+zaralX@users.noreply.github.com>
2025-06-04 02:25:00 +0800

    50 | director stats

---
+++ components/freshmarket/FMStatsCard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps({
+  title: String,
+  value: Number,
+})
+</script>
+
+<template>
+  <div class="w-full h-48 bg-neutral-900 rounded-lg shadow-lg border border-neutral-800 flex flex-col justify-center items-center">
+    <template v-if="$slots.default">
+      <slot />
+    </template>
+    <template v-else>
+      <div class="text-center px-4">
+        <p class="text-2xl">{{ title }}</p>
+        <p class="text-blue-500 text-lg">{{ value }}</p>
+      </div>
+    </template>
+  </div>
+</template>
