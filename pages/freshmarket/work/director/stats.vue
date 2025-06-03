b99b360980e68f6b3e015ff5647f946880817a14
zaralX <61970128+zaralX@users.noreply.github.com>
2025-06-04 02:46:49 +0800

    47 | director cells info

--- pages/freshmarket/work/director/stats.vue
+++ pages/freshmarket/work/director/stats.vue
@@ -9,12 +9,18 @@ definePageMeta({
 })
 
 const totalOrders = ref(0)
+const totalCells = ref(0)
+const usedCells = ref(0)
+const shopCells = ref(0)
 const totalCommissionBalance = ref(0)
 const totalSpentOnShops = ref(0)
 
 onMounted(async () => {
   let response = await http.get("/freshmarket/work/director/stats");
   totalOrders.value = response.data.totalOrders;
+  totalCells.value = response.data.totalCells;
+  usedCells.value = response.data.usedCells;
+  shopCells.value = response.data.shopCells;
   response = await http.get("/freshmarket/work/director/balance");
   totalCommissionBalance.value = response.data.totalCommissionBalance;
   totalSpentOnShops.value = response.data.totalSpentOnShops;
@@ -27,6 +33,11 @@ onMounted(async () => {
       <FMStatsCard title="Всего заказов" :value="totalOrders" />
       <FMStatsCard title="Всего АР с комки" :value="totalCommissionBalance" />
       <FMStatsCard title="Затрачено на создание магазинов" :value="totalSpentOnShops" />
+      <FMStatsCard />
+      <FMStatsCard title="Всего ячеек склада" :value="totalCells" />
+      <FMStatsCard title="Занято ячеек склада" :value="usedCells" />
+      <FMStatsCard title="Может быть занято ячеек склада" :value="shopCells" />
+      <FMStatsCard title="100% доступных ячеек" :value="totalCells - shopCells" />
     </div>
   </div>
 </template>
