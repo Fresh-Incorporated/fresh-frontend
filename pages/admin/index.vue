57680c91cdb67799305b1b098c4c66a40d7301cf
zaralX <61970128+zaralX@users.noreply.github.com>
2025-06-19 10:26:15 +0800

    70 | order stats

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -3,6 +3,7 @@ import AdminCard from "~/components/admin/AdminCard.vue";
 import {http} from "~/composables/useHttp"
 import AdminRegistrationsChart from "~/components/admin/AdminRegistrationsChart.vue";
 import AdminOnlineChart from "~/components/admin/AdminOnlineChart.vue";
+import AdminOrdersChart from "~/components/admin/AdminOrdersChart.vue";
 
 const {user} = useUser()
 
@@ -11,6 +12,7 @@ const shopBalances = ref(0)
 const totalSpentOnShops = ref(0)
 const totalUsers = ref(0)
 const registrations = ref([])
+const orders = ref([])
 
 onMounted(async () => {
   const response = await http.get("/admin/stats")
@@ -19,6 +21,7 @@ onMounted(async () => {
   shopBalances.value = response.data.totalBalanceShops
   totalUsers.value = response.data.totalUsers
   registrations.value = response.data.registrations
+  orders.value = response.data.orders
 })
 </script>
 
@@ -30,6 +33,7 @@ onMounted(async () => {
     <AdminCard title="Минимальный баланс карты" :value="userBalances + shopBalances" />
     <AdminCard title="Затрачено на создание магазинов" :value="totalSpentOnShops" />
     <AdminCard title="Регистраций" :value="totalUsers" />
+    <AdminOrdersChart v-model="orders" v-if="orders.length > 0" />
     <AdminRegistrationsChart v-model="registrations" v-model:current-registrations="totalUsers" v-if="registrations.length > 0" />
     <AdminOnlineChart />
   </div>
