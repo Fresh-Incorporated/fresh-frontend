55f92795ee1bf7302b135ed6440208770c5ef908
zaralX <61970128+zaralX@users.noreply.github.com>
2025-07-21 07:48:42 +0800

    unovis | custom chart component

--- components/admin/AdminOnlineChart.vue
+++ components/admin/AdminOnlineChart.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import {http} from "~/composables/useHttp";
 import {VisArea, VisCrosshair, VisLine, VisTooltip, VisXYContainer} from "@unovis/vue";
+import AreaLineChart, {type CategoryItem} from "~/components/goodies/charts/AreaLineChart.vue";
 
 const AreaChartData = ref<AreaChartItem[]>([])
 
@@ -10,16 +11,9 @@ interface AreaChartItem {
   users: number
 }
 
-const categories: Record<string, BulletLegendItemInterface> = {
+const categories: Record<string, CategoryItem> = {
   online: { name: 'Сейчас в сети', color: '#1def9f' },
   users: { name: 'Регистрации', color: '#631def' },
-}
-
-const x = (d: Object) => {
-  return new Date(d.date)
-}
-const y = (d: Object, key: string) => {
-  return d[key]
 }
 
 onMounted(() => {
@@ -37,60 +31,11 @@ const refresh = async () => {
     AreaChartData.value.shift()
   }
 }
-
-const svgDefs = computed(() => {
-  const createGradientWithHex = (id: number, color: string) => `
-    <linearGradient id="gradient${id}-${color}" gradientTransform="rotate(90)">
-      <stop offset="0%" stop-color="${color}" stop-opacity="1" />
-      <stop offset="100%" stop-color="${color}" stop-opacity="0" />
-    </linearGradient>
-  `; // БЛЯ НАДО КОМПОНЕНТ СДЕЛАТЬ А ТО ЭТО ПИЗДЕЦ ВСЁ ПОВТОРЯТЬ
-
-  return Object.entries(categories).map((color) =>
-      createGradientWithHex(color[0], color[1].color)
-  ).join("");
-});
-
-const template = (d: any) => {
-  const products = []
-  for (const category in categories) {
-    products.push(`<div class="flex items-center gap-2"><div style="background: ${categories[category].color}" class="w-2 h-2 rounded-full"></div> ${categories[category].name}: ${d[category]} АР</div>`)
-  }
-
-  return `<div>
-${formatDateAbsolute(d.date)}<br>
-${products.join("")}
-</div>`
-}
-
-const color = (d: any, i: number) => {
-  console.log(d)
-  return Object.values(categories).map(c => c.color)[i]
-}
 </script>
 
 <template>
   <ShCard class="col-span-4 !py-0 overflow-hidden gap-0">
-    <div class="h-8 flex justify-end items-center gap-4 mx-4">
-      <div class="flex-1">Real time</div>
-    </div>
-    <VisXYContainer :svg-defs="svgDefs" class="h-[224px]" :data="AreaChartData">
-      <VisTooltip />
-      <template v-for="(i, iKey) in categories" :key="iKey">
-        <VisLine
-            :x="x"
-            :y="(info) => y(info, iKey)"
-            :color="i.color"
-        />
-        <VisArea
-            :x="x"
-            :y="(info) => y(info, iKey)"
-            :color="`url(#gradient${iKey}-${i.color})`"
-            :opacity="0.5"
-        />
-      </template>
-      <VisCrosshair :color="color" :template="template" />
-    </VisXYContainer>
+    <AreaLineChart :categories="categories" v-model="AreaChartData" categories-switch />
   </ShCard>
 </template>
 
