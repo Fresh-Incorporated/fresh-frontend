4492179c9d1e60682e57c0979942a55890803aff
zaralX <61970128+zaralX@users.noreply.github.com>
2025-08-16 02:23:31 +0800

    freshmarket count in storage

--- pages/freshmarket/index.vue
+++ pages/freshmarket/index.vue
@@ -15,6 +15,7 @@ const search = ref("")
 const sort = ref("relevance")
 const tags = ref([])
 const selectedTags = ref([])
+const productsInStorage = ref([1, 256])
 
 const { isMobile, isMobileOrTablet } = useDevice()
 
@@ -64,7 +65,9 @@ const load = async () => {
         seed: searchSeed,
         search: search.value,
         sort: sort.value,
-        tags: selectedTags.value.map(tag => tag.id).join("_")
+        tags: selectedTags.value.map(tag => tag.id).join("_"),
+        min: productsInStorage.value[0],
+        max: productsInStorage.value[1] == 256 ? 10000 : productsInStorage.value[1],
       }
     });
 
@@ -102,6 +105,14 @@ const searchInput = async () => {
   }, 1000)
 }
 watch(sort, () => changedFilters())
+
+watch(productsInStorage, newProductsInStorage => {
+  setTimeout(async () => {
+    if (productsInStorage.value == newProductsInStorage) {
+      await changedFilters();
+    }
+  }, 1000)
+})
 </script>
 
 <template>
@@ -184,6 +195,17 @@ watch(sort, () => changedFilters())
                   </ShSelectGroup>
                 </ShSelectContent>
               </ShSelect>
+            </div>
+            <div class="space-y-2 flex flex-col min-w-64">
+              <ShLabel>Кол-во на складе</ShLabel>
+              <div class="flex items-centers gap-2">
+                <ShSlider
+                    v-model="productsInStorage"
+                    :max="256"
+                    :step="1"
+                />
+              </div>
+              <div class="text-xs opacity-75 grid grid-cols-2"><p>Мин. {{productsInStorage[0]}}</p> <p class="text-right">Макс. {{productsInStorage[1] == 256 ? "бесконечность" : productsInStorage[1]}}</p></div>
             </div>
           </div>
         </div>
